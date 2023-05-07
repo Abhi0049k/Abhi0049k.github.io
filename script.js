2acbacc16b51419570821a59a1d8a5c51e73cf6a
Abhi0049k <mangalamkumar2002@gmail.com>
2023-05-07 16:52:54 +0530

    Done with Frontend

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ let type = new Typed('.auto-input', {
     backDelay: 2000,
     loop: true
 })
+
+// Skill Section Start
+
+const progressBars = document.querySelectorAll('.inner-line');
+
+window.addEventListener('scroll', function(){
+    progressBars.forEach(function(progressBar){
+        const rect = progressBar.getBoundingClientRect();
+        if(rect.top < window.innerHeight && rect.bottom >= 0){
+            const width = progressBar.getAttribute('data-width');
+            progressBar.style.width = width+'%';
+        }
+    });
+});
+
+
+let navLinks = document.querySelectorAll('header nav ul li a');
+console.log(navLinks)
+let sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', ()=>{
+    const scrollPos = window.scrollY + 10
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link=>{
+                link.classList.remove('active');
+                if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
